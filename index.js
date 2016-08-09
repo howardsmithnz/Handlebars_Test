12de39d04845bb1657f6c5884f34895cd4d5512b
Howard Smith <howardsmithnz@gmail.com>
2016-08-09 12:00:28 +1200

    add static files

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', function (req, res) {
   res.send('Welcome to a Handlebars test app')
 })
 
+app.use('/public', express.static('public'))
+
 app.listen(3001, function () {
   console.log('Handlebars test running on port 3001')
 })

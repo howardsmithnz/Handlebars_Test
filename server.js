3fc62b1f335a1153eca45e21c61a17afc2b933cd
Howard Smith <howardsmithnz@gmail.com>
2016-08-09 14:34:24 +1200

    rename index.js to server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var hbs = require('express-hbs')
+
+var app = express()
+
+app.use('/public', express.static('public'))
+
+// Use `.hbs` for extensions and find partials in `views/partials`.
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views/partials'
+}))
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/views')
+
+// app.get('/', function (req, res) {
+//   res.send('Welcome to a Handlebars test app')
+// })
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.listen(3001, function () {
+  console.log('Handlebars test running on port 3001')
+})
